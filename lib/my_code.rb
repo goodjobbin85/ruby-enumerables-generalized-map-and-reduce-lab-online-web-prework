e95fae25c38acc277b54e778236951178a3c1ba3
thomas dubiel <td1729@hotmail.com>
2019-11-21 16:14:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,13 +11,17 @@ end
 
 def reduce(array, starter=nil) 
   if starter 
-    
-    new_value = starter
-    i = 0 
-    while i < array.size 
-      new_value += (yield(array[i])) 
-      i += 1 
-    end 
-    new_value 
+    new_value = starter 
+  else 
+    new_value = array[0] 
   end
+
+  i = 0 
+  while i < array.size 
+    new_value += (yield(array[i])) 
+    i += 1 
+  end 
+  new_value 
+  end 
+end
 
