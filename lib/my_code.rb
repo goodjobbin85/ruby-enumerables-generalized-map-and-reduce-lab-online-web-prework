fcb23bb637c3d6aacaff6631f09fa8943e8e7851
thomas dubiel <td1729@hotmail.com>
2019-11-21 16:13:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,6 +11,7 @@ end
 
 def reduce(array, starter=nil) 
   if starter 
+    
     new_value = starter
     i = 0 
     while i < array.size 
@@ -18,14 +19,5 @@ def reduce(array, starter=nil)
       i += 1 
     end 
     new_value 
-  else 
-    new_value = array[0] 
-    i = 1
-    while i < array.size 
-      new_value += (yield(array[i])) 
-      i += 1 
-    end 
-    new_value
   end
-end
 
