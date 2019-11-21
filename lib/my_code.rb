b9a2b8110237fc25272c83b837a55aa3fe34ab25
thomas dubiel <td1729@hotmail.com>
2019-11-21 16:06:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(array) 
+  new_array = []
+  i = 0 
+  while i < array.size 
+    new_array << (yield(array[i]))
+    i += 1 
+  end 
+  new_array
+end
+
+def reduce(array, starter=nil) 
+  if starter 
+    i = 0 
+    while i < array.size 
+      (yield(array[i])) 
+      i += 1 
+    end 
+    starter 
+  end
+end
+
